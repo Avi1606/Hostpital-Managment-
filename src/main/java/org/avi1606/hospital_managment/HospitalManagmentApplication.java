ec103c024b18f37de56f4c4ccff71c94c524cafa
Avikumar <aviiiprajapati16@gmail.com>
2025-09-19 23:26:58 +0530

    Add configuration for ModelMapper and WebSecurity; set up security filter chain and context path

--- src/main/java/org/avi1606/hospital_managment/HospitalManagmentApplication.java
+++ src/main/java/org/avi1606/hospital_managment/HospitalManagmentApplication.java
@@ -12,8 +12,4 @@ public class HospitalManagmentApplication {
         SpringApplication.run(HospitalManagmentApplication.class, args);
     }
 
-    @Bean
-    public ModelMapper modelMapper() {
-        return new ModelMapper();
-    }
 }
---
+++ src/main/java/org/avi1606/hospital_managment/config/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+package org.avi1606.hospital_managment.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeHttpRequests(auth ->auth
+                        .requestMatchers("/api/v1/admin/**").authenticated()
+                        .requestMatchers("api/v1/public/**").permitAll()
+                )
+                .formLogin(Customizer.withDefaults());
+        return httpSecurity.build();
+    }
+}
